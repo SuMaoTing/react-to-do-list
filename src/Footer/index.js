bf3df29bb1b6fdcca8bcf5fa12c39c7089c67dd2
Ethan Su <84558082+SuMaoTing@users.noreply.github.com>
2022-06-17 22:29:42 +0800

    init

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import styles from './index.css';
 
 const Footer = () =>(
-    <p className="Footer">made by <a href="https://sumaoting.github.io/MyWebsite/"> SuMaoTing </a> at 2022/5</p>
+    <>
+        <p className="Footer">made by <a href="https://sumaoting.github.io/MyWebsite/"> SuMaoTing </a> at 2022/5 <a href="https://github.com/SuMaoTing/react-to-do-list">Code</a></p> 
+    </> 
 );
 
 export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Header from './Header';
 import Footer from './Footer';
 import TodoList from './TodoList';
+import './index.css'
 
 ReactDOM.render(
   <>
